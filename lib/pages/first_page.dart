111239c6a150c3b6dc96773b5afd38ed08709f5e
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-02-07 00:24:14 +0530

    ListTile

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -16,9 +16,16 @@ class FirstPage extends StatelessWidget {
         width: 200,
 
         backgroundColor: const Color.fromARGB(255, 138, 101, 239),
-        
-
-           
+        child: Column(
+          children: [
+            //common to place a drawer header here
+            DrawerHeader(child: Icon(Icons.favorite, size: 50)),
+            // home page list tile
+            ListTile(leading: Icon(Icons.home), title: Text("H O M E ")),
+            ListTile(leading: Icon(Icons.home), title: Text("S E T T I N G ")),
+            ListTile(leading: Icon(Icons.home), title: Text("M E N U E  ")),
+          ],
+        ),
       ),
       body: Center(
         child: ElevatedButton(
